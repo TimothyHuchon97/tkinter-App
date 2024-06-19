1eb55da36c0dd4dc7f5e5a5675231007bd74bedc
TimothyHuchon97 <timothy.huchon.contact@gmail.com>
2024-06-20 01:18:39 +0200

    add: (feature) added 2 checkbox to display markers by wine categories

--- main.py
+++ main.py
@@ -20,17 +20,36 @@ class WineAppView:
 
         self.style = ttk.Style()
         self.style.configure("TFrame", background="#202020")
+        self.style.configure("TCheckbutton", background="#202020", foreground="white", font=("Helvetica", 14))
 
         self.frame_side_bar = ttk.Frame(self.root, style="TFrame", width=150)
         self.frame_btns = ttk.Frame(self.frame_side_bar, style="TFrame")
         self.frame_content = ttk.Frame(self.root, style="TFrame")
+        self.frame_bottom_bar = ttk.Frame(self.frame_content, style="TFrame")
 
         self.frame_side_bar.pack(side="left", fill="y")
         self.frame_btns.pack(side="left")
         self.frame_content.pack(side="left", fill="both", expand=True)
+        self.frame_bottom_bar.pack(side="bottom", fill="x")
 
         self.map_widget = TkinterMapView(self.frame_content)
-        self.map_widget.pack(fill="both", expand=True, padx=20, pady=20)
+        self.map_widget.pack(fill="both", expand=True, padx=15, pady=(15, 0))
+
+        self.check_red_wine = tk.BooleanVar()
+        self.check_white_wine = tk.BooleanVar()
+
+        self.check_btn_red_wine = ttk.Checkbutton(self.frame_bottom_bar, text="Tinto", style="TCheckbutton", 
+                                                  variable=self.check_red_wine, onvalue=True, offvalue=False, 
+                                                  command=self.controller.display_wine_markers_by_category)
+        self.check_btn_white_wine = ttk.Checkbutton(self.frame_bottom_bar, text="Blanco", style="TCheckbutton", 
+                                                  variable=self.check_white_wine, onvalue=True, offvalue=False, 
+                                                  command=self.controller.display_wine_markers_by_category)
+
+        self.check_btn_red_wine.grid(column=0, row=0, pady=15)
+        self.check_btn_white_wine.grid(column=1, row=0, pady=15)
+
+        self.frame_bottom_bar.columnconfigure(0, weight=1)
+        self.frame_bottom_bar.columnconfigure(1, weight=1)
 
     def create_btns(self, wines_locations_infos):
         for key, value in wines_locations_infos.items():
@@ -50,6 +69,9 @@ class WineAppController:
         self.view = WineAppView(root, self)
         self.set_up_buttons()
         self.zoom_on_location(40.416775, -3.703790, 6)
+    
+    def test(self):
+        print("Hello")
 
     def zoom_on_location(self, latitude, longitude, zoom):
         self.view.map_widget.set_position(latitude, longitude)
@@ -66,8 +88,21 @@ class WineAppController:
         wine_icon = ImageTk.PhotoImage(ICON_RED_WINE) if wine_location_info[1] == "Tinto" else ImageTk.PhotoImage(
             ICON_WHITE_WINE)
 
-        self.view.map_widget.set_marker(latitude, longitude, icon=wine_icon)
+        self.view.map_widget.set_marker(latitude, longitude, icon=wine_icon, command=lambda: self.test())
         self.zoom_on_location(latitude, longitude, 7)
+
+    def display_wine_markers_by_category(self):
+        red_wine = self.view.check_red_wine.get()
+        white_wine = self.view.check_white_wine.get()
+
+        self.view.map_widget.delete_all_marker()
+        for key, value in self.model.wines_location_info.items():
+            if (red_wine and value[1] == "Tinto") or (white_wine and value[1] == "Blanco"):
+                wine_icon = ImageTk.PhotoImage(ICON_RED_WINE) if value[1] == "Tinto" \
+                    else ImageTk.PhotoImage(ICON_WHITE_WINE)
+                wine_marker = self.view.map_widget.set_marker(value[0][0], value[0][1], icon=wine_icon)
+                wine_marker.set_text(key)
+                self.zoom_on_location(40.416775, -3.703790, 6)
 
 
 def main():
